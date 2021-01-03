35c211b02498d6368df1815c701c8465253a0a07
asomejay <museum114@naver.com>
2021-01-03 22:35:06 +0900

    :sparkles: add cover page

--- brainstorm/settings.py
+++ brainstorm/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,6 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '^8!nrbrtg4dr(a43du28*)4nxo)x+f2eo_^-y(d&c7#4(rlb7b'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
